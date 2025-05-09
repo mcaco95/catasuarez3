ba4bf48146c9c5c0a5046b2c43c0f09aef8c4d88
mcaco95 <mcallistersimon95@gmail.com>
2025-05-09 11:19:57 -0600

    Chore: Final client updates including TikTok links and layout adjustments3

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { motion, useScroll, useTransform, useSpring, useMotionValueEvent } from 'framer-motion';
+import { motion, useScroll, useSpring, useMotionValueEvent } from 'framer-motion';
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { HiMail } from 'react-icons/hi';
